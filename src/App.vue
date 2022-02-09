04e0ba1cd15d9db8acc366068a469735533baf81
dohyun <shindohyunx@gmail.com>
2022-02-09 23:02:45 +0900

    [추가] toolbar, navigation 연결

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,12 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6">
@@ -83,6 +57,7 @@ export default {
   name: 'App',
 
   data: () => ({
+    drawer: false,
     items: [
       { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/dashboard'},
       { title: 'Grid-System', icon: 'mdi-view-dashboard', to: '/grid-system'},
