36f25e932b7534e6a201cf1120299e5e75aeadfe
dohyun <shindohyunx@gmail.com>
2022-02-09 22:36:10 +0900

    [추가] vuetify, 테스트 view 추가
    vue-router 를 vuetify 보다 먼저 추가해아한다.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Dashboard Page
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+    
+  }
+}
+</script>
---
+++ src/views/GridSystem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    GridSystem Page
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+    
+  }
+}
+</script>
